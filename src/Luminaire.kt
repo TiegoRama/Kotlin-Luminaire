c2cc0c9ba8c12e94977717ed26c1449385f4675d
TiegoRama <ramatiego@gmail.com>
2024-03-29 11:37:54 +0100

    2ème commit

--- src/Luminaire.kt
+++ src/Luminaire.kt
@@ -1,2 +1,67 @@
-class Luminaire {
+class Luminaire: Eclairage {
+    val ampoule1 = AmpouleLed()
+    val ampoule2 = AmpouleIncandescente()
+    val ampoule3 = AmpouleTube()
+    val ampoule4 = AmpouleIncandescente()
+    var etat1: Int  = 0
+    var etat2: Int = 0
+    var etat3: Int  = 0
+    var etat4: Int  = 0
+    fun ampoule1() {
+        this.etat1 = 0
+    }
+    fun ampoule2() {
+        this.etat2 = 0
+    }
+    fun ampoule3() {
+        this.etat3 = 0
+    }
+    fun ampoule4() {
+        this.etat4 = 0
+    }
+    private fun probaPanne(): Int {
+        val alea = (0..100).random() // generated random from 0 to 100 included
+        return if (alea < 10) {
+            -1
+        } else 0
+    }
+
+    override fun allumer() {
+    ampoule1.allumer()
+    ampoule2.allumer()
+    ampoule3.allumer()
+    ampoule4.allumer()
+    }
+
+
+    override fun eteindre() {
+        ampoule1.eteindre()
+        ampoule2.eteindre()
+        ampoule3.eteindre()
+        ampoule4.eteindre()
+    }
+
+    override fun intensifier() {
+    ampoule1.intensifier()
+    ampoule2.intensifier()
+    ampoule3.intensifier()
+    ampoule4.intensifier()
+    }
+
+    override   fun diminuer() {
+        ampoule1.diminuer()
+        ampoule2.diminuer()
+        ampoule3.diminuer()
+        ampoule4.diminuer()
+    }
+
+    override fun etat(): Int {
+    return  ampoule1.etat() and ampoule2.etat() and ampoule3.etat() and ampoule4.etat()
+
+
+
+    }
+    override fun toString(): String {
+        return this.javaClass.name + " : " + ampoule1.etat() +" "+ ampoule2.etat() +" " +ampoule3.etat() +" "+ ampoule4.etat()
+}
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,65 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val eclairage = AmpouleIncandescente();
+    /**
+     * Cycles d'éclairage et d'extinction d'une ampoule
+     */
+    var nbCycles = 0;
+    do{
+        eclairage.allumer();
+        while(eclairage.etat() > 0 ){
+            eclairage.diminuer();
+        }
+        println(eclairage.toString());
+        nbCycles ++;
+    }while (nbCycles < 1000 && eclairage.etat() != -1);
+    println("nombre de cycles incandescente "+nbCycles);
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    /** Ampoule Tube  */
+
+
+    val eclairagetube = AmpouleTube()
+    /**
+     * Cycles d'éclairage et d'extinction d'une ampoule
+     */
+    nbCycles = 0
+    do{
+        eclairagetube.allumer();
+        while(eclairagetube.etat() > 0 ){
+            eclairagetube.diminuer();
+        }
+        println(eclairagetube.toString());
+        nbCycles ++;
+    }while (nbCycles < 1000 && eclairagetube.etat() != -1);
+    println("nombre de cycles tube "+nbCycles);
+
+    /** Ampouleled  */
+
+    val eclairageled = AmpouleLed()
+    /**
+     * Cycles d'éclairage et d'extinction d'une ampoule
+     */
+    nbCycles = 0
+    do{
+        eclairageled.allumer();
+        while(eclairageled.etat() > 0 ){
+            eclairageled.diminuer();
+        }
+        println(eclairageled.toString());
+        nbCycles ++;
+    }while (nbCycles < 1000 && eclairageled.etat() != -1);
+    println("nombre de cycles led "+nbCycles);
+/** Luminaire  */
+    var ampoule1 = Luminaire()
+    nbCycles = 0
+    do{
+        ampoule1.allumer();
+        while(ampoule1.etat() > 0 ){
+            ampoule1.diminuer();
+        }
+        println(ampoule1.toString());
+        nbCycles ++;
+    }while (nbCycles < 1000 && ampoule1.etat() != -1);
+    println("nombre de cycles  "+nbCycles);
 }
